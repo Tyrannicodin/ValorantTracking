c58db013e83cf78cf1c51f41dd6a4a9e4579a2f3
Tyrannicodin16 <86689800+Tyrannicodin@users.noreply.github.com>
2022-01-20 22:29:32 +0000

    V1.1.0
    Added match history window.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #Import requirements
 from json import dump, load
-from tkinter import BooleanVar, Button, Label, TclError, Tk, Checkbutton
+from tkinter import BooleanVar, Button, Label, TclError, Tk, Checkbutton, Toplevel, Frame
+from functools import partial
 from client import Client
 from time import time as timestamp
 
@@ -22,12 +23,34 @@ cli = Client()
 root = Tk()
 root.wm_title("Valorant tracker")
 
+#Add match to data
+def add_match(dat):
+    with open("data.json", "r") as f:
+        file = load(f)
+    file["matches"].append(dat)
+    with open("data.json", "w") as f:
+        dump(file, f)
+
+#History
+def openHistory():
+    historyWindow = Toplevel(root)
+    for game in cli.get_history(MAX_MATCHES):
+        f = Frame(historyWindow)
+        game = cli.parse_match(game)
+        text=f"Map: {game['map']}      Queue: {game['typle']}\n"
+        text += str([p["stats"]["kills"] for p in game["players"] if p["ID"] == cli.puuid][0])
+        l = Label(f, text=text)
+        l.grid(column=0, row=0)
+        add = Button(f, text="Add match", command=partial(add_match, game))
+        add.grid(column=1, row=0)
+        f.pack(anchor="n")
+
 #Add buttons, labels, etc.
 trackLabel = Label(root, text="Tracking enabled: ")
 trackOn = BooleanVar()
 trackOnCheck = Checkbutton(root, variable=trackOn)
 trackOnCheck.select()
-history = Button(root, text=f"View history ({MAX_MATCHES} matches)")
+history = Button(root, text=f"View history ({MAX_MATCHES} matches)", command=openHistory)
 view = Button(root, text="View data")
 
 trackLabel.grid(row=0, column=0)
@@ -45,11 +68,7 @@ while True:
         next_check = timestamp() + CHECK_INTERVAL
         dat = cli.parsed_latest()
         if dat["ID"] != lastID:
-            with open("data.json", "r") as f:
-                file = load(f)
-            file["matches"].append(dat)
-            with open("data.json", "w") as f:
-                dump(file, f)
+            add_match(dat)
     elif not trackOn:
         next_check = timestamp() + CHECK_INTERVAL*10
     try:
