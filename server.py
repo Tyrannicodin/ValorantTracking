2dfb1ec145252e0e7654b828465f1d190ac3381e
Tyrannicodin16 <86689800+Tyrannicodin@users.noreply.github.com>
2022-01-22 18:40:42 +0000

    Run this to host a server

---
+++ server.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+from json import load, dump, loads
+
+from itsdangerous import exc
+from numpy import where
+
+app = Flask(__name__)
+
+@app.post("/upload")
+def upload():
+    headers = request.headers
+    data = loads(request.data)
+    try:
+        with open(f"{headers['Region']}.json", "r") as f:
+            locdat = load(f)
+    except FileNotFoundError:
+        with open(f"{headers['Region']}.json", "w") as f:
+            dump({"matches":[], "tracked":[]}, f)
+            locdat = {"matches":{}, "tracked":[]}
+    save = []
+    for match in data:
+        if not match["ID"] in locdat["tracked"]:
+            match["submitted_by"] = [headers["Puuid"]]
+            match.pop("completed")
+            match["rounds"] = len(match["rounds"])
+            for player in match["players"]:
+                player.pop("damage")
+                player.pop("xpModifiers")
+                player["stats"].pop("abilities")
+            save.append(match)
+            locdat["tracked"].append(headers["Puuid"])
+        else:
+            i = locdat["matches"].index([m for m in locdat["matches"] if m["ID"] == match["ID"]][0])
+            locdat["matches"][i]["submitted_by"].append(headers["Puuid"])
+    locdat["matches"].extend(save)
+    with open(f"{headers['Region']}.json", "w") as f:
+        dump(locdat, f)
+    return {"status":200}
+
+app.run()
