1a2ae5e5873272c7d6df84ec167acc3772081cf8
Tyrannicodin16 <86689800+Tyrannicodin@users.noreply.github.com>
2022-01-22 13:35:03 +0000

    Added viewing stats

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 #Import requirements
 from json import dump, load
-from tkinter import BooleanVar, Button, Label, TclError, Tk, Checkbutton, Toplevel, Frame
+from tkinter import BooleanVar, Button, Label, OptionMenu, StringVar, TclError, Tk, Checkbutton, Toplevel, Frame
 from requests import get
 from functools import partial
 from client import Client
 from time import time as timestamp
+from PIL.Image import new
+from PIL.ImageDraw import Draw
+from PIL.ImageFont import truetype
+from random import randint
+from collections import Counter
+from PIL.ImageTk import PhotoImage
 
 #Set constans
 MAX_MATCHES = 10
@@ -57,13 +63,96 @@ def openHistory():
             add.grid(column=1, row=0)
         f.pack(anchor="n")
 
+#View stats
+pillowFont = truetype("arial.ttf", 7)
+def generate_bar(xNames:list, values:list):
+    base = new("RGB", (len(xNames)*32+40, max(values)*20+40), (255, 255, 255))
+    bDraw = Draw(base)
+    bDraw.line((20, 20, 20, base.height-20, base.width-20, base.height-20), (0, 0, 0), 1)
+    for name, value, i in zip(xNames, values, range(len(xNames))):
+        bDraw.rectangle(((i+1)*32+4, base.height-21, (i+1)*32+20, ((value+1)*-20)+base.height), (0, 255, 255))
+        bDraw.text(((i+1)*32+4, base.height-20), str(name), (0, 0, 0), pillowFont)
+    return base
+
+def generate_line(xNames:list, values:list):
+    base = new("RGB", (len(xNames)*32+40, max(values)*20+40), (255, 255, 255))
+    bDraw = Draw(base)
+    bDraw.line((20, 20, 20, base.height-20, base.width-20, base.height-20), (0, 0, 0), 1)
+    points = [20, base.height-20]
+    for i, value, name in zip(range(len(xNames)), values, xNames):
+        points.append((i+1)*32+21)
+        points.append(((value+1)*-20)+base.height)
+        bDraw.text(((i+2)*20, base.height-20), str(name), (0, 0, 0), pillowFont)
+    bDraw.line(points, (0, 255, 255), 1)
+    return base
+
+def generate_pie(xNames:list, values:list):
+    base = new("RGB", (250, 250), (255, 255, 255))
+    bDraw = Draw(base)
+    multiplier = 360/sum(values)
+    values = [value*multiplier for value in values]
+    previous = 0
+    for value in values:
+        bDraw.pieslice((10, 10, 240, 240), previous, previous+value, (randint(0, 256), randint(0, 256), randint(0, 256)))
+        previous += value
+    return base
+
+def get_info(info):
+    if info == "kills":
+        with open("data.json", "r") as f:
+            dat = load(f)["matches"]
+        tlist = []
+        [tlist.extend(a) for a in [p["players"] for p in dat]]
+        c = [a["stats"]["kills"] for a in tlist if a["ID"] == cli.puuid]
+        return [i+1 for i in range(len(c))], c
+    elif info == "maps":
+        with open("data.json", "r") as f:
+            dat = load(f)["matches"]
+        c = Counter([m["map"] for m in dat])
+        c = c.items()
+        return [map[0] for map in c], [map[1] for map in c]
+
+    elif info == "agents":
+        with open("data.json", "r") as f:
+            dat = load(f)["matches"]
+        tlist = []
+        [tlist.extend(a) for a in [p["players"] for p in dat]]
+        c = Counter([get(f"https://valorant-api.com/v1/agents/{a['agent']}").json()["data"]["displayName"] for a in tlist if a["ID"] == cli.puuid])
+        c = c.items()
+        return [agent[0] for agent in c], [agent[1] for agent in c]
+
+GRA_OPTIONS = {
+    "bar": generate_bar,
+    "pie":generate_pie,
+    "line":generate_line
+}
+
+def openView():
+    def gen_and_open():
+        data = get_info(typeVar.get())
+        win = Toplevel(viewWindow)
+        im=PhotoImage(GRA_OPTIONS[graphVar.get()](*data))
+        winIcon = Label(win, image=im)
+        winIcon.image = im
+        winIcon.pack()
+        
+    viewWindow = Toplevel(root)
+    typeVar = StringVar()
+    typeSelector = OptionMenu(viewWindow, typeVar, "kills", "maps", "agents")
+    graphVar = StringVar()
+    graphSelector = OptionMenu(viewWindow, graphVar, "bar", "pie", "line")
+    gen = Button(viewWindow, text="Generate", command=gen_and_open)
+    typeSelector.grid(column=0, row=0)
+    graphSelector.grid(column=1, row=0)
+    gen.grid(column=1, row=1)
+
 #Add buttons, labels, etc.
 trackLabel = Label(root, text="Tracking enabled: ")
 trackOn = BooleanVar()
 trackOnCheck = Checkbutton(root, variable=trackOn)
 trackOnCheck.select()
 history = Button(root, text=f"View history ({MAX_MATCHES} matches)", command=openHistory)
-view = Button(root, text="View data")
+view = Button(root, text="View data", command=openView)
 
 trackLabel.grid(row=0, column=0)
 trackOnCheck.grid(row=0, column=1)
@@ -81,6 +170,7 @@ while True:
         dat = cli.parse_match()
         if dat["ID"] != lastID:
             add_match(dat)
+            lastID = dat["ID"]
     elif not trackOn:
         next_check = timestamp() + CHECK_INTERVAL*10
     try:
